14724b9dc4a2b444737a95e616ba45f601372430
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-06-14 09:51:12 +0530

    added translations

---
+++ translations.js
@@ -0,0 +1,20 @@
+export const SelectYourRole = {
+    'en': 'Select Your Role.',
+    'hi': 'अपनी भूमिका चुने'
+}
+export const RequestScribe = {
+    'en': 'Request a Scribe',
+    'hi': 'मुंशी (स्क्राइब) निवेदन करे'
+}
+export const Volunteer = {
+    'en': 'Volunteer',
+    'hi': 'वालंटियर (स्वयंसेवा)'
+}
+export const LoginText = {
+    'en': 'Log in',
+    'hi': 'लॉग इन'
+}
+export const EnterYourEmail = {
+    'en': 'Enter your email address',
+    'hi': 'ईमेल डाले '
+}
