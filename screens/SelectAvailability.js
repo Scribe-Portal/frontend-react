cb92d643520367cab6f840ac3b239e2e6545c572
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-11-02 18:04:32 +0530

    select availability improved

--- screens/SelectAvailability.js
+++ screens/SelectAvailability.js
@@ -3,18 +3,15 @@ import React, { Component, useState } from 'react';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Platform} from 'react-native';
 import { useSelector } from 'react-redux';
-import { useFirestore } from 'react-redux-firebase';
+import { useFirestore, useFirestoreConnect } from 'react-redux-firebase';
 
 import firebase from '@react-native-firebase/app';
-import { CalendarList } from 'react-native-calendars';
+import { Calendar } from 'react-native-calendars';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#D4D4D4",
-        justifyContent: 'center',
-
-
     },
     input: {
         margin: 10,
@@ -27,9 +24,10 @@ const styles = StyleSheet.create({
 
     },
     upperHalf: {
+        
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        margin: 20,
+        justifyContent: 'space-around'
     },
     lowerHalf: {
         flex: 1,
@@ -49,6 +47,7 @@ const styles = StyleSheet.create({
         borderColor: "#616161",
         borderRadius: 10,
         padding: 5,
+        margin: 10,
         alignItems: 'center',
         borderWidth: 3,
     },
@@ -72,8 +71,14 @@ const styles = StyleSheet.create({
     }
 
 });
+
 function SelectAvailability({ navigation }) {
     let firestore = useFirestore()
+    const uid = useSelector(state => state.userAppSettings.uid)
+    useFirestoreConnect(() => [
+        {collection:`scribes/${uid}/availableDays`, storeAs: 'availableDays'}
+    ])
+    const availableDays = useSelector(state => state.firestore.data.availableDays)
     let [name, setName] = useState('')
     let [gender, setGender] = useState('')
     let [DOB, setDOB] = useState('')
@@ -85,7 +90,6 @@ function SelectAvailability({ navigation }) {
     const [recentlyDisabled1, setRecentlyDisabled1] = useState(false)
     const [recentlyDisabled2, setRecentlyDisabled2] = useState(true)
     const [errText, setErrText] = useState('')
-    const uid = useSelector(state => state.userAppSettings.uid)
     const lang = useSelector(state => state.userAppSettings.lang)
     const onChange = (event, selectedDate) => {
         const currentDate = selectedDate || date
@@ -96,55 +100,68 @@ function SelectAvailability({ navigation }) {
         setShow(true)
     }
     let maximumDate = new Date()
+    let minimumDate = new Date()
     maximumDate.setDate(maximumDate.getDate() + 60)
     return (
         <ScrollView>
 
             <View style={styles.container}>
-                <View style={styles.upperHalf} >
-                    <CalendarList
-                        scrollEnabled={true}
-                        markedDates={markedDates}
-                        // Enable horizontal scrolling, default = false
-                        horizontal={true}
-                        // Enable paging on horizontal, default = false
-                        pagingEnabled={true}
-                        // Set custom calendarWidth.
-                        calendarWidth={320}
-                        maxDate = {maximumDate}
-                        onDayPress={(date) => {
-                            // console.log(date.dateString)
-                            setDate(date.dateString)
-                            firestore.collection(`dateslots/${date.toDateString()}`)
-                                .doc(uid)
-                                .get()
-                                .then((userDoc) => {
-                                    if (userDoc.exists) {
+                
+                <Calendar
+                    enableSwipeMonths={true}
+                    horizontal={true}
+                    pagingEnabled={true}
+                    minDate={minimumDate}
+                    maxDate={maximumDate}
+                    markedDates={availableDays}
+                    onDayPress={(date) => {
+                        setDate(date)
+                        firestore.collection('dateslots')
+                            .doc(date.dateString)
+                            .collection('available')
+                            .doc(uid)
+                            .get()
+                            .then((userDoc) => {
+                                if (userDoc.exists) {
+                                    console.log("this doc exists")
+                                    setRecentlyDisabled1(true)
+                                    setRecentlyDisabled2(false)
+                                }
+                                else {
+                                    console.log("na doesnt exist")
+                                    
+                                    firestore.collection('dateslots')
+                                    .doc(date.dateString)
+                                    .set({
+                                        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                                    })
+                                    setRecentlyDisabled1(false)
+                                    setRecentlyDisabled2(true)
+                                }
+                            })
+                            .catch((err) => {
+                                console.log('something seriously wrong 4 ' + err)
+                            })
+                    }}
 
-                                        setRecentlyDisabled1(true)
-                                        setRecentlyDisabled2(false)
-                                    }
-                                    else {
-
-                                        setRecentlyDisabled1(false)
-                                        setRecentlyDisabled2(true)
-                                    }
-                                })
-                                .catch((err) => {
-                                    console.log('something seriously wrong 4 ' + err)
-                                })
-                        }}
-
-                    />
-                </View>
+                />
+                
                 <TouchableOpacity style={styles.AvailButton}
-                    disabled={recentlyDisabled1}
+                    
                     onPress={() => {
 
-                        firestore.collection(`dateslots/${date.toDateString()}`)
+                        firestore.collection('dateslots')
+                            .doc(date.dateString)
+                            .collection('available')
                             .doc(uid)
-                            .update({
+                            .set({
                                 setAt: firebase.firestore.FieldValue.serverTimestamp(),
+                            })
+                        firebase.collection('scribes')
+                            .doc(uid)
+                            .collection('availableDays')
+                            .add({
+                                day: date.dateString,
                             })
                         setRecentlyDisabled1(true)
                         setRecentlyDisabled2(false)
@@ -161,7 +178,9 @@ function SelectAvailability({ navigation }) {
                     disabled={recentlyDisabled2}
                     onPress={() => {
 
-                        firestore.collection(`dateslots/${date.toDateString()}`)
+                        firestore.collection('dateslots')
+                            .doc(date.dateString)
+                            .collection('available')
                             .doc(uid)
                             .delete()
                             .catch((err) => {
@@ -179,11 +198,8 @@ function SelectAvailability({ navigation }) {
                     </Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.AvailButton}
-                    
                     onPress={() => {
                         navigation.goBack()
-
-
                     }}
                 >
                     <Text style={styles.t1}>
