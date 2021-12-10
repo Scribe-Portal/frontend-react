7426c1e9373184e250101d7114a5b59185c12383
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-12-10 10:15:23 +0530

    formatting

--- screens/RequestsA.js
+++ screens/RequestsA.js
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
         top: 0,
         color: "#19939A",
         alignSelf: "flex-start",
-        
+        margin: 5,
         fontSize: 30,
         fontWeight: '700',
     },
@@ -31,22 +31,22 @@ const styles = StyleSheet.create({
         padding: 10,
         backgroundColor: "#BFE9E7",
     },
-    requestRoot:{
+    requestRoot: {
     },
     examName: {
         fontSize: 20,
         fontWeight: '500',
     },
 });
-function Request({req_id}) {
-    const request = useSelector(({firestore: { data }})=> data.requests && data.requests[req_id])
+function Request({ req_id }) {
+    const request = useSelector(({ firestore: { data } }) => data.requests && data.requests[req_id])
     const navigation = useNavigation()
-    
-    
+
+
     return (
         <View style={styles.requestRoot}>
 
-            <TouchableOpacity style={styles.requestBox} onPress={() => navigation.navigate("RequestPageA", {req_id: req_id, })}>
+            <TouchableOpacity style={styles.requestBox} onPress={() => navigation.navigate("RequestPageA", { req_id: req_id, })}>
                 <Text style={styles.examName}>{request.examName}</Text>
                 {/* <Text style={styles.examDate}>{request.examDate.toLocaleStrrin}</Text> */}
             </TouchableOpacity>
@@ -54,9 +54,9 @@ function Request({req_id}) {
 
     )
 }
-function Requests({uid}){
-    const requests = useSelector(state => state.firestore.ordered.requests.filter(req => (req.status==='accepted')))
-    if (!isLoaded(requests)){
+function Requests({ uid }) {
+    const requests = useSelector(state => state.firestore.ordered.requests.filter(req => (req.status === 'accepted')))
+    if (!isLoaded(requests)) {
         return (
             <Text style={styles.text1}>
                 Loading...
@@ -64,32 +64,32 @@ function Requests({uid}){
         )
 
     }
-    if (isEmpty(requests)){
+    if (isEmpty(requests)) {
         return (
             <Text>
                 No Requests
             </Text>
         )
     }
-    return requests.map(({id: id}, ind) => (
-        <Request req_id={id}  key={`${ind}-${id}`}/>
+    return requests.map(({ id: id }, ind) => (
+        <Request req_id={id} key={`${ind}-${id}`} />
     ))
 }
 function RequestsA() {
     const lang = useSelector(state => state.userAppSettings.lang)
-    
+
     const uid = useSelector(state => state.userAppSettings.uid)
-    
+
     return (
         <View style={styles.container}>
-            
 
-                <Text style={styles.text1}>
+
+            <Text style={styles.text1}>
                 Volunteer Search Successful
-                </Text>
-                
-            
-            <Requests uid={uid}/>
+            </Text>
+
+
+            <Requests uid={uid} />
 
 
         </View>
