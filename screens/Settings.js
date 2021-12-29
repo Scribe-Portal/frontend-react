7a7608320abf96313789ed8015d747aa5b23000e
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2021-12-29 20:14:28 +0530

    the nss form

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,10 +1,10 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Alert } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Alert, Linking } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeScribeStatus, changeTempUid, changeUid } from '../reducers/userAppSettingsReducer';
 
-
+var NSSFormURL = "https://docs.google.com/forms/d/e/1FAIpQLSdoK6B8czhAmA3MTE4Q5V92w-V2N5suKy9MH5hLxugoeTOMZw/viewform?usp=sf_link"
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -98,6 +98,14 @@ function Settings({ route, navigation }) {
 
         )
     }
+    const showFail = () => {
+        return Alert.alert(
+            "Can't Open the Form",
+            "Please try again later",
+            
+
+        )
+    }
     return (
         <View style={styles.container}>
         
@@ -137,6 +145,26 @@ function Settings({ route, navigation }) {
                         Upload Documents
                     </Text>
                 </TouchableOpacity>
+                {isItAScribe ?
+                    <TouchableOpacity style={styles.button1}
+                        onPress={() => {
+                            Linking.canOpenURL(NSSFormURL).then(supported => {
+                                if (supported) {
+                                    Linking.openURL(NSSFormURL);
+                                } else {
+                                    showFail()
+                                }
+                            });
+                        }}
+
+                    >
+                        <Text style={styles.t1}>
+
+                            Fill NSS Form
+                        </Text>
+                    </TouchableOpacity>
+
+                : null}
                 <TouchableOpacity style={styles.button1}
                     onPress={showLogoutDialog}
                 >
