14307f30092bf1aef257b0e860089defc3356b44
mrityunjai01 <mrityunjaisingh3333@gmail.com>
2022-01-02 12:16:03 +0530

    about nss simplified

--- screens/AboutNSS.js
+++ screens/AboutNSS.js
@@ -1,15 +1,10 @@
 /* eslint-disable prettier/prettier */
-import React, { Component, useState } from 'react';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Platform, Button, Image } from 'react-native';
-import { useSelector } from 'react-redux';
-import { useFirestore } from 'react-redux-firebase';
-import { FillInfoText } from '../translations'
-import { PLACEHOLDERS } from '@babel/types';
+import React, { Component,  } from 'react';
+
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
+
 import { ScrollView } from 'react-native';
-import { useDispatch } from 'react-redux';
-import { changeUid } from '../reducers/userAppSettingsReducer';
-useSelector
+
 const styles = StyleSheet.create({
 
     container: {
@@ -22,35 +17,6 @@ const styles = StyleSheet.create({
 
 
     },
-    c1: {
-        marginHorizontal: 20,
-        marginVertical: 10,
-    },
-    middle_spacing: {
-        flexGrow: 1,
-        // backgroundColor: "red",
-    },
-    c2: {
-        marginHorizontal: 20,
-        marginVertical: 10,
-
-    },
-    input: {
-        marginVertical: 3,
-        height: 40,
-        borderRadius: 5,
-        backgroundColor: "white"
-    },
-    upperHalf: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    lowerHalf: {
-        flex: 1,
-        margin: 20,
-        justifyContent: 'space-around'
-    },
     text1: {
         color: "#19939A",
         fontSize: 30,
@@ -60,19 +26,6 @@ const styles = StyleSheet.create({
         color: "#19939A",
         fontSize: 20,
         fontWeight: '400',
-    },
-    tsmall: {
-
-    },
-    radioRoot: {
-        backgroundColor: "white",
-        flexDirection: 'row',
-        padding: 10,
-        marginVertical: 7,
-        borderRadius: 5,
-    },
-    radioText: {
-        margin: 7,
     },
     FillInfoButton: {
         backgroundColor: '#19939A',
@@ -87,19 +40,6 @@ const styles = StyleSheet.create({
         color: "#FFFFFF",
         fontSize: 30
     },
-    dateOfBirthPicker: {
-
-        backgroundColor: '#19939A',
-        borderRadius: 10,
-        padding: 5,
-        height: 40,
-        alignItems: 'center',
-        marginVertical: 10,
-    },
-    textInsideDoBPicker: {
-        color: "#FFFFFF",
-        fontSize: 20,
-    }
 });
 function AboutNSS({ navigation }) {
     return (
@@ -118,7 +58,7 @@ function AboutNSS({ navigation }) {
                     </Text>
                     
                     <Text style={styles.text2}>
-                    NSS IITD has a volunteer base of around 2000 students belonging to various disciplines and degrees of IIT Delhi. We work on a diverse range of social issues including blood donation, teaching, environmental issues, etc. We also collaborate with several NGO's in and around IIT Delhi on working on these issues. 
+                        NSS IITD has a volunteer base of around 2000 students belonging to various disciplines and degrees of IIT Delhi. We work on a diverse range of social issues including blood donation, teaching, environmental issues, etc. We also collaborate with several NGO's in and around IIT Delhi on working on these issues. 
                     </Text>
 
                 </View>
